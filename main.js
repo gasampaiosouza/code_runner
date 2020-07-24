152e344c6545e432935bfaead84051a76695e54d
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-24 01:10:02 -0300

    refactoring code

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const toggle_mode = (element) => (
-  $('.toggle--icon').toggleClass('dark'), element.classList.toggle('dark-mode')
-);
+const switchTheme = (element) => {
+  const iconClass = 'dark';
+  const elementClass = 'dark-mode';
+
+  $('.toggle--icon').toggleClass(iconClass);
+  element.classList.toggle(elementClass);
+};
 
 const $codeArea = $('#code--area');
 
@@ -13,11 +17,12 @@ const $result = $('#result--text');
 
 // change console.log default behavior
 (function () {
-  var oldLog = console.log;
+  const oldLog = console.log;
 
   console.log = function (message) {
     const consoleMessage = `
-			<span class='no-color'>Console is saying:</span> <br/><span class='console'>${message}</span>
+      <span class='no-color'>Console is saying:</span><br />
+      <span class='console'>${message}</span>
 		`;
     $result.html(consoleMessage);
 
@@ -26,16 +31,15 @@ const $result = $('#result--text');
   };
 })();
 
-const checkBrackets = (element) => {
-  const elementID = $(`#${element.id}`);
-
-  const ElementValue = elementID.val();
+const checkBrackets = ({ id }) => {
+  const textarea = $(`#${id}`);
+  const elementValue = textarea.val();
 
   const setCaretPosition = (textAreaID, caretPos) => {
     const textAreaElement = document.getElementById(textAreaID);
 
     if (textAreaElement.createTextRange) {
-      let range = textAreaElement.createTextRange();
+      const range = textAreaElement.createTextRange();
       return range.move('character', caretPos), range.select();
     }
 
@@ -50,40 +54,46 @@ const checkBrackets = (element) => {
 
   const symbols = ['()', '[]', '{}', '<>'];
 
-  symbols.forEach((symbolGroup) => {
-    const symbol = symbolGroup.split('');
+  symbols.forEach((pair) => {
+    const symbol = pair.split('');
+    const valueLength = elementValue.length;
 
-    const lastChar = ElementValue.slice(
-      ElementValue.length - 1,
-      ElementValue.length
-    );
+    const lastChar = elementValue.slice(valueLength - 1, valueLength);
 
     if (lastChar === symbol[0]) {
-      elementID.val(ElementValue + symbol[1]);
-
-      setCaretPosition(element.id, ElementValue.length);
-    }
+      textarea.val(elementValue + symbol[1]);
+      setCaretPosition(id, valueLength);
+    } else return;
   });
 };
 
-const runCodeButton = $('#run--code');
+const $runCodeButton = $('#run--code');
 
 // hide things on code run
-runCodeButton.click(() => showCode($codeArea, $result, $resultPlaceholder));
+$runCodeButton.click(() => showCode($codeArea, $result, $resultPlaceholder));
 
-document.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13 && e.ctrlKey)
-    return showCode($codeArea, $result, $resultPlaceholder);
-});
+const runOnCtrlEnter = (event) => {
+  const keyCode = event.keyCode || event.which;
+  const enterKey = 13;
 
-function showCode(input, output, display_none = '') {
+  const enter = keyCode == enterKey;
+  const ctrl = event.ctrlKey;
+
+  if (enter && ctrl) showCode($codeArea, $result, $resultPlaceholder);
+  else return;
+};
+
+document.addEventListener('keyup', runOnCtrlEnter);
+
+const showCode = (input, output, elementToHide = '') => {
   try {
     const code = input.val();
 
     output.html(eval(code));
   } catch (err) {
     const errorMessage = `
-			<span class='no-color'>Console and it's errors...</span> <br> <span class='console'>${err}</span>
+      <span class='no-color'>Console and it's errors...</span><br />
+      <span class='console'>${err}</span>
 		`;
 
     output.html(errorMessage);
@@ -91,10 +101,8 @@ function showCode(input, output, display_none = '') {
 
   const hideElement = (element) => element.css('display', 'none');
 
-  return hideElement(display_none);
-}
+  return hideElement(elementToHide);
+};
 
-// clear console, so it doesn't appear in the screen;
-setTimeout(() => {
-  console.clear();
-}, 200);
+// clear console, so it doesn't appear on the screen
+setTimeout(() => console.clear(), 200);
